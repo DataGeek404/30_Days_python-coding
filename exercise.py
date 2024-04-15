04dc79f0368cd85fabcd977ef156c94dac589f0e
DataGeek404 <techjayprivate@gmailcom>
2024-04-15 15:45:26 +0300

    my first exercise

---
+++ exercise.py
@@ -0,0 +1,12 @@
+#datatypes
+a=([10, 20, 30, "Jay"],[10, 20, 30, "Jay"] )
+b=6.0 + 3j
+c={
+    "name": "Jay",
+    "age" : 22,
+    "Course" : "Software Engineering"
+}
+
+print(type(a))
+print(type(b))
+print(type(c))
