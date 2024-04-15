713e807cb388db5ac3632ef9d47a65f796156060
DataGeek404 <techjayprivate@gmailcom>
2024-04-15 14:40:11 +0300

    My corrected commit Day1

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+"""a=3
+b=a+3
+print(b)
+"""
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List - Python list is an ordered collection which allows to store different data type items. A list is similar to an array in JavaScript.
+print(type({'name':'Asabeneh'})) # Dictionary- A Python dictionary object is an unordered collection of data in a key value pair format.
+print(type({9.8, 3.14, 2.7}))    # Set- A set is a collection of data types similar to list and tuple. Unlike list and tuple, set is not an ordered collection of items. Like in Mathematics, set in Python stores only unique items.
+print(type((9.8, 3.14, 2.7)))    # Tuple - A tuple is an ordered collection of different data types like list but tuples can not be modified once they are created. They are immutable.
